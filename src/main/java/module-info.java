6670a6c3a3a43b1f97da46eec22274f7dad78202
reipo38 <v03a01raik@pm.me>
2024-05-18 21:14:19 +0300

    Changed positions from LinkedList to HashSet

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.snakegame {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
     opens com.snakegame to javafx.fxml;
     exports com.snakegame;
